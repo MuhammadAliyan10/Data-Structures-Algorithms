2ba34dd8ece41e218ac250b2dcb5429a086c727a
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2024-05-15 11:50:14 +0500

    Add Assignment

---
+++ Java/Stack/Assignment/Stack.java
@@ -0,0 +1,84 @@
+package Java.Stack.Assignment;
+
+import java.util.Arrays;
+
+public class Stack {
+    static int stackValueLeft = -1;
+    static int stackArray[] = new int[100];
+    static int stackValueRight = stackArray.length;
+
+    public void showStackArray() {
+        System.out.println(Arrays.toString(stackArray));
+    }
+
+    public void showStackIndex() {
+        System.out.println(stackValueLeft);
+        System.out.println(stackValueRight);
+    }
+
+    private void pushfromLeft(int number) {
+        if (stackValueLeft < stackValueRight) {
+            ++stackValueLeft;
+            stackArray[stackValueLeft] = number;
+        } else {
+            System.out.println("No space.");
+        }
+    }
+
+    private void pushfromRight(int number) {
+        if (stackValueRight > stackValueLeft) {
+            --stackValueRight;
+            stackArray[stackValueRight] = number;
+        } else {
+            System.out.println("No space.");
+
+        }
+    }
+
+    private void popOfLeft() {
+        if (stackValueLeft >= 0) {
+            --stackValueLeft;
+            stackArray[stackValueLeft + 1] = 0;
+        } else {
+            System.out.println("No value in the stack.");
+        }
+    }
+
+    private void popOfRight() {
+        if (stackValueRight < 100) {
+            stackArray[stackValueRight] = 0;
+            ++stackValueRight;
+        } else {
+            System.out.println("No value in the stack.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.pushfromLeft(5);
+        stack.showStackArray();
+        stack.showStackIndex();
+        stack.pushfromRight(4);
+        stack.showStackArray();
+        stack.showStackIndex();
+        stack.pushfromRight(3);
+        stack.showStackArray();
+        stack.showStackIndex();
+        stack.pushfromRight(2);
+        stack.showStackArray();
+        stack.showStackIndex();
+        stack.popOfLeft();
+        stack.showStackArray();
+        stack.showStackIndex();
+        stack.popOfRight();
+        stack.showStackArray();
+        stack.showStackIndex();
+        stack.popOfRight();
+        stack.showStackArray();
+        stack.showStackIndex();
+        stack.popOfRight();
+        stack.showStackArray();
+        stack.showStackIndex();
+
+    }
+}
