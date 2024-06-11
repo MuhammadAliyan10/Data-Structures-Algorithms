1f52b4105f110a0480a6de2a4b4d2642e30782aa
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2024-06-11 11:10:53 +0500

    Add Binary Tree

---
+++ Java/Tree/BinaryTree.java
@@ -0,0 +1,100 @@
+package Java.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+}
+
+public class BinaryTree {
+    static int idx = -1;
+
+    public Node buildTree(int nodes[]) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
+        }
+        Node newNode = new Node(nodes[idx]);
+        newNode.left = buildTree(nodes);
+        newNode.right = buildTree(nodes);
+        return newNode;
+    }
+
+    public void preOrder(Node root) { // ! Time complexity is O(n)
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+    public void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data);
+        inOrder(root.right);
+    }
+
+    public void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data);
+
+    }
+
+    public void traversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currentNode = q.remove();
+            if (currentNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currentNode.data + " ");
+                if (currentNode.left != null) {
+                    q.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    q.add(currentNode.right);
+                }
+
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree bt = new BinaryTree();
+        Node root = bt.buildTree(nodes);
+        bt.traversal(root);
+
+    }
+}
