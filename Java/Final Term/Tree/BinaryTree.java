db16c3f11f39e334570b50029579ddb1d46bae2b
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2024-08-07 14:39:59 +0500

    Add Tree

---
+++ Java/Final Term/Tree/BinaryTree.java
@@ -0,0 +1,92 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Tree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+        public static void preOrder(Node root) { // Time complexity is O(n)
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data);
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public static void inOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+
+        public static void postOrder(Node root) { // Time complexity is O(n)
+            if (root == null) {
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+        public static void levelOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            queue.add(null);
+
+            while (!queue.isEmpty()) {
+                Node current = queue.poll();
+                if (current == null) {
+                    System.out.println();
+                    if (!queue.isEmpty()) {
+                        queue.add(null);
+                    }
+                } else {
+                    System.out.print(current.data + " ");
+                    if (current.left != null) {
+                        queue.add(current.left);
+                    }
+                    if (current.right != null) {
+                        queue.add(current.right);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+        Node root = Tree.buildTree(nodes);
+        Tree.levelOrder(root);
+    }
+}
